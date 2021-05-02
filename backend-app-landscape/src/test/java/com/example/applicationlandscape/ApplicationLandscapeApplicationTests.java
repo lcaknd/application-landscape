dd358398e54ff2e98df24d0986f568cbbbfadb9f
madzia <magdaapekackaa@gmail.com>
2021-05-02 13:04:50 +0200

    initial fontend and backend

---
+++ backend-app-landscape/src/test/java/com/example/applicationlandscape/ApplicationLandscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationlandscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLandscapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
