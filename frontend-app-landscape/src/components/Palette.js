eaf360e8dd17fe3b5ba584100353767a8d602ea6
madzialenk1 <magdaapekackaa@gmail.com>
2021-05-11 23:15:53 +0200

    added new shapes

--- frontend-app-landscape/src/components/Palette.js
+++ frontend-app-landscape/src/components/Palette.js
@@ -30,11 +30,22 @@ function initPalette(){
         allowDelete: false,
         allowZoom: false,  
         model: new go.GraphLinksModel([ 
-          { category: "Start", text: "Start", },
-          { text: "Step" },
+          { category: "MicroformProcessing", text: "Start", },
+          { text: "Step",category:"FivePointedStar" },
           { category: "Conditional", text: "???" },
-          { category: "End", text: "End" },
-          { category: "Comment", text: "Comment" }
+          { category: "Comment", text: "Comment" },
+          { category: "", text: "RoundedRectagle" },
+          { category: "Hexagon", text: "Hexagon" },
+          { category: "DataStorage", text: "DataStorage" },
+          { category: "DiskStorage", text: "DiskStorage" },
+          { category: "ExternalOrganization", text: "ExternalOrganization" },
+          { category: "ExternalProcess", text: "ExternalProcess"},
+          { category: "MicroformProcessing", text: "MicroformProcessing"},
+          { category: "Ellipse", text: "Ellipse"},
+          { category: "Circle", text: "Circle"},
+          { category: "Diamond", text: "Diamond"},
+
+
         ])
       });
     return myPalette;
@@ -46,12 +57,21 @@ const Palette =()=>{
         <ReactPalette
         initPalette={initPalette}
         divClassName='myPaletteDiv'
-        nodeDataArray={[{ key: 0, text: 'Shit1', color: "cyan" },
-        { key: 1, text: 'Shit2', color: "powderblue" },
-        { key: 2, text: 'Shit3',color: "lightblue" },
-        { key: 3, text: 'Shit4', color: "deepskyblue" },
-        { key: 4, text: 'Shit5', color: "aquamarine"},
-        { key: 5, text: 'Shit6', color: "turquoise" }
+        nodeDataArray={[{ key: 0, text: 'Hello1', color: "cyan",category:"FivePointedStar" },
+        { key: 1, text: 'Hello2', color: "powderblue",category:"Database"},
+        { key: 2, text: 'Hello3',color: "lightblue",category:"Comment" },
+        { key: 3, text: 'Hello3',color: "lightblue",category:"RoundedRectagle" },
+        { key: 4, text: 'Hello3',color: "lightblue",category:"Hexagon" },
+        { key: 5, text: 'Hello3',color: "lightblue",category:"DataStorage" },
+        { key: 6, text: 'Hello3',color: "lightblue",category:"DiskStorage" },
+        { key: 7, text: 'Hello3',color: "lightblue",category:"ExternalOrganization" },
+        { key: 8, text: 'Hello3',color: "lightblue",category:"ExternalProcess" },
+        { key: 9, text: 'Hello3',color: "lightblue",category:"MicroformProcessing" },
+        { key: 10, text: 'Hello3',color: "lightblue",category:"Ellipse" },
+        { key: 11, text: 'Hello3',color: "lightblue",category:"Circle" },
+        { key: 12, text: 'Hello3',color: "lightblue",category:"Diamond" },
+        
+
         ]}
         />
         </>
