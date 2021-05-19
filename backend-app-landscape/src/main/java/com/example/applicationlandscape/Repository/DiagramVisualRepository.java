5fbd16304aaa69cf33e99bc387e90c1da5bb2753
madzialenk1 <magdaapekackaa@gmail.com>
2021-05-19 13:59:32 +0200

    first entity, set the database

---
+++ backend-app-landscape/src/main/java/com/example/applicationlandscape/Repository/DiagramVisualRepository.java
@@ -0,0 +1,9 @@
+package com.example.applicationlandscape.Repository;
+
+import com.example.applicationlandscape.Entity.DiagramVisual;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiagramVisualRepository extends JpaRepository<DiagramVisual, Long> {
+}
