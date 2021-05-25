b0c451871417538d7d9f06eb52ceaf10485056b5
huzeifa716 <74735005+huzeifa716@users.noreply.github.com>
2021-05-25 20:20:11 +0200

    Update Home.js

--- frontend-app-landscape/src/components/Home.js
+++ frontend-app-landscape/src/components/Home.js
@@ -3,22 +3,20 @@ import styled from 'styled-components'
 import {MenuItem1, MenuItem2, MenuItem3, MenuItem4,MenuItem_logo } from "./MenuItems"
 import DarkMode from './DarkMode.js';
 import './DarkMode.css';
+import * as FiIcons  from "react-icons/fi";
 
-const Nav = styled.div`
-background: #cfeef5;
-height: 80px;
-display: flex;
-justify-content: flex-start;
-align-items: center;
-border-radius: 5px;
-`;
+const Nav = styled.div`;
+
+ `;
 
 
 
 const navbar =()=> {
     return (
         <>
+        
         <Nav > 
+        <div class="nav">
         <div>
         <h1 className="nbar-logo">{MenuItem_logo.map((item) => {
                             return(
@@ -80,9 +78,19 @@ const navbar =()=> {
                             )
                         })}
                         </div>
+                    <div class="darkModeIcon">
+                        <div class="sun">
+                        <FiIcons.FiSun />
+                        </div>
+                        <div class="moon">
+                        <FiIcons.FiMoon />
+                        </div>
+                        </div>
+                        
                         <DarkMode/>
-                    
+                        </div>     
         </Nav>
+        
         
         <h1>This is Home. </h1>
          <h1><a href="landscape">Click here </a>to start or continue working on the landscape.</h1>
